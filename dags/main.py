31a9da03e7303c4374c43e145c9f00205d4949dc
Amit Mukati <amitmukati1729@gmail.com>
2026-05-09 11:23:30 -0400

    Added data insertion, cleaning, and transformation related files in dags/datawarehouse folder

--- dags/main.py
+++ dags/main.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
-
+from datawarehouse.dwh import staging_table, core_table
 
 local_tz = pendulum.timezone("America/New_York")
 
@@ -21,6 +21,7 @@ default_args = {
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
 
+# DAG 1: produce_json
 with DAG(
     dag_id='produce_json',
     default_args=default_args,
@@ -37,3 +38,20 @@ with DAG(
 
     #define dependencies
     playlist_id >> video_ids >> extract_data >> save_to_json_task
+
+
+# DAG 2: update_db
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    schedule="0 15 * * *",
+    catchup=False,
+) as dag:
+
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+
+    # Define dependencies
+    update_staging >> update_core
